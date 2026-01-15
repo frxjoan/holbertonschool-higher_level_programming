867b31b2d98ad209bd8363ade36e39072f1af0fb
frxjoan <12224@holbertonstudents.com>
2026-01-15 10:03:58 +0100

    pycode

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-	ld = abs(number)
-	print(ld % 10, end = "")
-	return ld % 10
+    ld = abs(number)
+    print(ld % 10, end="")
+    return ld % 10
