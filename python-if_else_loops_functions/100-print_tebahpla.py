3d8a9db40e294fa4b135919073d455718045e6c5
frxjoan <12224@holbertonstudents.com>
2026-01-15 10:16:41 +0100

    100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 for i in range(ord("z"), ord("a") - 1, -1):
     print(
-        "{}".format(
-            chr(i)
-            if (ord('z') - i) % 2 == 0
-            else chr(i - 32)
+        "{:c}".format(
+            i if (ord('z') - i) % 2 == 0 else i - 32
         ),
         end=""
     )
