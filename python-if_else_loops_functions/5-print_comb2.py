8492fc330c83dd80e73708226e367e8e308d6293
frxjoan <12224@holbertonstudents.com>
2026-01-15 09:32:50 +0100

    5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-	if (i != 0):
-		if (i < 10):
-			print(", 0{}".format(i), end = "")
-		else:
-			print(", {}".format(i), end = "")
-	else:
-		print("00", end = "")
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
