28a143106c9c63e5d85170297ec9ee805439a9f9
frxjoan <12224@holbertonstudents.com>
2026-01-16 10:07:58 +0000

    sandbox

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ import hidden_4
 
 if __name__ == "__main__":
     new_dict = sorted(hidden_4.__dict__.keys())
-    for el in new_dict:
-        if not el.startswith("__"):
-            print(el)
+    for words in new_dict:
+        if not words.startswith("__"):
+            print(words)
