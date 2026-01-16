22ae48a37664aacb5be4190ff655e3594ed6ff0f
frxjoan <12224@holbertonstudents.com>
2026-01-16 10:59:26 +0100

    e

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import sys
 
-result = 0
 if __name__ == "__main__":
+    result = 0
     argc = len(sys.argv) - 1
 
-for i in range(argc):
-    result += int(sys.argv[i + 1])
-print("{}".format(result))
+    for i in range(argc):
+        result += int(sys.argv[i + 1])
+    print("{}".format(result))
