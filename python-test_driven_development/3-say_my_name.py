ad424360bfcae23be7ff659befa61ffc6f4cefc1
frxjoan <12224@holbertonstudents.com>
2026-01-21 14:02:28 +0100

    help

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -9,11 +9,7 @@ It validates argument types.
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    Prints My name is <first_name> <last_name>.
-
-    Raises a TypeError if first_name or last_name is not a string.
-    """
+    """Prints My name is <first_name> <last_name>."""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
