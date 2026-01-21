205e4853e9cfcc75dcf3e50e8982dc219a8348d6
frxjoan <12224@holbertonstudents.com>
2026-01-21 10:57:02 +0100

    accept bool

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -14,9 +14,9 @@ def add_integer(a, b=98):
     a and b must be integers or floats.
     Floats are casted to integers before addition.
     """
-    if isinstance(a, bool) or not isinstance(a, (int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if isinstance(b, bool) or not isinstance(b, (int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     if isinstance(a, float) and (a != a or a in (float("inf"), float("-inf"))):
