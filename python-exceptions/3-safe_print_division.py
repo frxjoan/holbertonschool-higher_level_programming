f58c3f8048f7f71fb46fdc4444ed7ef22ba22bb0
frxjoan <12224@holbertonstudents.com>
2026-01-21 10:05:07 +0100

    print

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = 0
+    result = None
     try:
         result = a / b
-    except ValueError:
-        result = None
     except ZeroDivisionError:
         result = None
+    except TypeError:
+        result = None
     finally:
-        return result
+        print("{}".format(result))
+    return result
