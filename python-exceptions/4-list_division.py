2cab2b9157ec574887e9e71d0768624263c73611
frxjoan <12224@holbertonstudents.com>
2026-01-21 10:12:18 +0100

    finally missing and some prints

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,9 +4,15 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
-        except (ZeroDivisionError, TypeError):
+        except TypeError:
             result = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            result = 0
+            print("division by 0")
         except IndexError:
             result = 0
-        lst.append(result)
+            print("out of range")
+        finally:
+            lst.append(result)
     return lst
