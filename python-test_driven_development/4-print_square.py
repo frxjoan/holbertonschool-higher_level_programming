4ed90e87f05c48489274d59f2066dcf7f67e2de7
frxjoan <12224@holbertonstudents.com>
2026-01-21 14:35:19 +0100

    size == 0 case

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -22,5 +22,9 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
+    if size == 0:
+        print()
+        return
+
     for _ in range(size):
         print("#" * size)
