cb99ef11f97ed6e2073a6cbcb52d8c348c9cbbce
frxjoan <12224@holbertonstudents.com>
2026-01-29 09:00:44 +0100

    1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
-class Square:
+"""
+Module square
 
+Defines a Square class.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): size of the square.
+    """
     def __init__(self, size):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
