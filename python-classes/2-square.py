cf652238c04c06d739cfc27d8582a733cc3772cb
frxjoan <12224@holbertonstudents.com>
2026-01-29 09:01:54 +0100

    2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python3
+"""
+Module square
+
+Defines a Square class with size validation.
+"""
+
+
 class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): size of the square.
+    """
 
     def __init__(self, size=0):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): Size of the square (must be >= 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
