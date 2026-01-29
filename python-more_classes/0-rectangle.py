33ca9d3962d82286c3e9ef8006bfa770effa9511
frxjoan <12224@holbertonstudents.com>
2026-01-29 09:16:03 +0100

    0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+Module rectangle
+
+Defines a Rectangle class.
+"""
+
+
 class Rectangle:
+    """
+    Represents a rectangle.
+
+    This class currently does not define any attributes or methods.
+    """
     pass
