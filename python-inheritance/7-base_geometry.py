9796d8bfeff331699f98e973076a409b19892385
frxjoan <12224@holbertonstudents.com>
2026-02-04 14:35:57 +0100

    7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,7 +10,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Rectangle class that inherits from BaseGeometry."""
-        if type(value) is not int:
+        if not isinstance(value, int) or isinstance(value, bool):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
