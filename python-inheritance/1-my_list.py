f2aa02266d62fe48e12d3a73530245c051a33c1a
frxjoan <12224@holbertonstudents.com>
2026-02-04 14:13:16 +0100

    1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''This function returns a list'''
+
+
 class MyList(list):
+    '''This function returns a list'''
     def print_sorted(self):
+        '''This function returns a list'''
         print(sorted(self))
