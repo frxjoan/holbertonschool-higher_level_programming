e2de80ce7605ff310f2ac382b225cd888ccd6797
frxjoan <12224@holbertonstudents.com>
2026-02-11 09:57:43 +0100

    5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""Module for saving an object to a JSON file."""
 import json
-'''json file'''
 
 
 def save_to_json_file(my_obj, filename):
-    '''json to string'''
-    with open(filename, 'w', encoding="utf-8") as f:
-        f.write(json.dumps(my_obj))
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
