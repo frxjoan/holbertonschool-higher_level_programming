05ce5007675923c9d8fdd255f56f2257a2ba24c2
frxjoan <12224@holbertonstudents.com>
2026-03-03 10:51:50 +0100

    full project 2 with README

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Create a new user in MySQL and grant all privileges to that user.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
