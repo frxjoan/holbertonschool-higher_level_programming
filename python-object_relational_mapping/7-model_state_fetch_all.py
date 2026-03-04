6fb30d8c6d73326ab44c18a9a7704b25e8577aac
frxjoan <12224@holbertonstudents.com>
2026-03-04 10:24:23 +0100

    fix

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,26 +1,33 @@
-'''Fetches all State objects from the database and prints their id and name'''
+#!/usr/bin/python3
+"""Fetch all State objects from the database and print their id and name."""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
-if __name__ == "__main__":
 
+def main():
+    """Connect to the database and display all State objects."""
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(username, password, database),
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            username, password, database
+        ),
         pool_pre_ping=True
     )
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
 
     session.close()
+
+
+if __name__ == "__main__":
+    main()
