a4930894d9dc5435f76b09305244cecd9adacff0
frxjoan <12224@holbertonstudents.com>
2026-03-04 10:18:10 +0100

    add join

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -22,7 +22,12 @@ def main():
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    cur.execute(
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities "
+        "INNER JOIN states ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC"
+    )
 
     for row in cur.fetchall():
         print(row)
