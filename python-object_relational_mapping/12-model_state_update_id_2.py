b1ffb241463bc1a5780d261e01a3faa71f2f2a88
frxjoan <12224@holbertonstudents.com>
2026-03-04 11:13:09 +0100

    12

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.id == 2).first()
-    if state:
+    state = session.query(State).get(2)
+    if state is not None:
         state.name = "New Mexico"
         session.commit()
 
