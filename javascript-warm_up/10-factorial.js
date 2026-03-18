d185fb18c4107c4a6f3926203f9290d4e1fdd200
frxjoan <12224@holbertonstudents.com>
2026-03-18 14:22:33 +0100

    tab

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 function factorial (n) {
-	if (isNaN(n) || n <= 1) {
-		return 1;
-	}
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
 
-	return n * factorial(n - 1);
+  return n * factorial(n - 1);
 }
 
 const number = parseInt(process.argv[2], 10);
