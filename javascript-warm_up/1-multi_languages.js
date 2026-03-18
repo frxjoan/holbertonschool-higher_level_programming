83d0a25e048ef773d6c09c3e687ee06100924da6
frxjoan <12224@holbertonstudents.com>
2026-03-18 12:38:41 +0100

    1

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar = 'C is fun';
+console.log(myVar);
+const myVar2 = 'JavaScript is amazing';
+console.log(myVar2);
+const myVar3 = 'Python is cool';
+console.log(myVar3);
